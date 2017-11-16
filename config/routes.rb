71af9d4e8e8f5f9089f5c0ef00d92b8c4936df97
nelsongomez-remolona <nelsongomez-remolona>
2017-11-16 10:43:18 -0800

    new front edit

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,7 @@ Rails.application.routes.draw do
   get 'main/case4'
   get 'main/case5'
   get 'main/case6'
+  get 'main/case7'
   get 'main/zingchart'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html

226b53a23ad29352571079857547d53d2b4f0723
nelsongomez-remolona <nelsongomez.remolona@gmail.com>
2019-02-16 00:51:49 -0500

    trying to fix route

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,8 @@
 Rails.application.routes.draw do
   get 'main/index'
-
   get 'main/case1'
-
   get 'main/case2'
-
   get 'main/case3'
-
   get 'main/case4'
   get 'main/case5'
   get 'main/case6'
@@ -14,8 +10,7 @@ Rails.application.routes.draw do
   get 'main/case8'
   get 'main/case9'
   get 'main/case10'
-  
-  get 'main/case11'
+  get 'main/case12'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :main
   root 'main#index'
